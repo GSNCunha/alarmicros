7a9342e0fa7c400d690a734528239aaf342f243e
Therocyn <vitor.simondesouza@gmail.com>
2023-07-13 16:33:12 -0300

    square waves of 2hz, 400hz, 800hz added

---
+++ ondas_quadradas/ondas_quadradas.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+#ifndef _ondas_quadradas_
+#define _ondas_quadradas_
+
+void onda_800hz();
+void onda_400hz();
+void onda_2hz();
+
+#endif // MATRIZ_H_INCLUDED
