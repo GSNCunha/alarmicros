24a142c57647731e37a1893737d0b5d9ee3ed472
Vinicius Zortea <vinizortea@gmail.com>
2023-07-22 16:00:00 -0300

    isso

---
+++ lcdtimerteclado/lcd_dale/sensores.c
@@ -0,0 +1,15 @@
+/*
+ * sensores.c
+ *
+ * Created: 22/07/2023 10:26:45
+ *  Author: Vini
+ */ 
+#include "timers.h"
+
+int sensores_ativados = 0;
+
+void armar_sensores(){
+	
+	
+}
+
---
+++ lcdtimerteclado/lcd_dale/sensores.h
@@ -0,0 +1,16 @@
+/*
+ * sensores.h
+ *
+ * Created: 22/07/2023 10:26:56
+ *  Author: Vini
+ */ 
+
+
+#ifndef SENSORES_H_
+#define SENSORES_H_
+
+
+
+
+
+#endif /* SENSORES_H_ */
